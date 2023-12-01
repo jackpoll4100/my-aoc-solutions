24ed1067cb06dfd1cd5bf9aedaca3cadfb599636
jackpoll4100 <43215677+jackpoll4100@users.noreply.github.com>
2023-12-01 13:50:48 -0700

    Create aoc-day1-p1.js

---
+++ aoc-day1-p1.js
@@ -0,0 +1,9 @@
+// Put csv input here:
+let input = '';
+input = input.split(',');
+let sum = 0;
+input.forEach((ind)=>{ 
+	const n = ind.replace(/\D/g, '');
+	sum += parseInt(`${n[0]}${n[n.length-1]}`); 
+});
+console.log(sum);
