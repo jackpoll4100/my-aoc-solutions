86a4bd88e347aa4b29f2a6bf19c1e0b69c791f1d
jackpoll4100 <43215677+jackpoll4100@users.noreply.github.com>
2023-12-04 14:09:12 -0700

    Create aoc-day2-p2.js

---
+++ aoc-day2-p2.js
@@ -0,0 +1,28 @@
+// Requires lodash
+// Put | separated input here:
+input = input.split('|');
+let out = 0;
+input.forEach((game)=>{
+	const gameTokens = game.split(':');
+  const gameLine = gameTokens[1].split(';')
+  let gameObj = {
+  	red: 0,
+    green: 0,
+    blue: 0
+  };
+  const colors = ['red', 'green', 'blue'];
+  gameLine.forEach((line)=>{
+  	const tempLine = line.split(',');
+    colors.forEach((color)=>{
+    	const colorArr = _.filter(tempLine, (item) => {
+    		return item.includes(color);
+    	});
+      if (colorArr.length) {
+      	const reducedColor = parseInt(colorArr[0].replace(/\D/g, ''));
+        gameObj[color] = reducedColor > gameObj[color] ? reducedColor : gameObj[color];
+      }
+    });
+  });
+  out += gameObj.red*gameObj.green*gameObj.blue;
+});
+console.log(out);
