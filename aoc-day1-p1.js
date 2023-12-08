129a0ddab076dc55cf52b28c187cc641549dfe54
jackpoll4100 <43215677+jackpoll4100@users.noreply.github.com>
2023-12-08 12:41:55 -0700

    fix formatting

--- aoc-day1-p1.js
+++ aoc-day1-p1.js
@@ -2,8 +2,8 @@
 let input = '';
 input = input.split(',');
 let sum = 0;
-input.forEach((ind)=>{ 
-	const n = ind.replace(/\D/g, '');
-	sum += parseInt(`${n[0]}${n[n.length-1]}`); 
+input.forEach((ind) => {
+    const n = ind.replace(/\D/g, '');
+    sum += parseInt(`${n[0]}${n[n.length-1]}`);
 });
 console.log(sum);
