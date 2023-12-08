fec07fd4bcb241a101769ec6aafd235db46de1ec
jackpoll4100 <43215677+jackpoll4100@users.noreply.github.com>
2023-12-08 12:46:43 -0700

    fix formatting

--- aoc-day4-p2.js
+++ aoc-day4-p2.js
@@ -3,20 +3,20 @@ let input = '';
 input = input.split(',');
 let multArr = new Array(223).fill(1);
 let out = 0;
-input.forEach((card, index)=>{
-	let myCard = card.split(':');
-  myCard = myCard[1].split('|');
-  const correct = myCard[0].split(' ');
-  const myNums = myCard[1].split(' ');
-  let cardScore = 0;
-  myNums.forEach((num)=>{
-  	if (!isNaN(parseInt(num))){
-    	cardScore += correct.includes(num) ? 1 : 0;
+input.forEach((card, index) => {
+    let myCard = card.split(':');
+    myCard = myCard[1].split('|');
+    const correct = myCard[0].split(' ');
+    const myNums = myCard[1].split(' ');
+    let cardScore = 0;
+    myNums.forEach((num) => {
+        if (!isNaN(parseInt(num))) {
+            cardScore += correct.includes(num) ? 1 : 0;
+        }
+    });
+    for (let x = 0; x < cardScore; x++) {
+        multArr[index + x + 1] += multArr[index];
     }
-  });
-  for (let x = 0; x < cardScore; x++){
-  	multArr[index+x+1] += multArr[index];
-  }
-  out += (multArr[index]*cardScore)+1;
+    out += (multArr[index] * cardScore) + 1;
 });
 console.log(out);
