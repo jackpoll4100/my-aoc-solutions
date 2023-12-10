9d89a36b56a358e6b0b7b55d53c4c9e42a763558
jackpoll4100 <43215677+jackpoll4100@users.noreply.github.com>
2023-12-10 15:32:59 -0700

    Create aoc-day9-p2.js

---
+++ aoc-day9-p2.js
@@ -0,0 +1,38 @@
+// Put csv input here:
+let input = ''.split(',');
+
+let chain = [];
+
+function extrapolate(vals) {
+    let returnVal = true;
+    vals.forEach((val) => {
+        if (val) {
+            returnVal = false;
+        }
+    });
+    if (returnVal) {
+        return;
+    }
+    let extrapolated = [];
+    for (let x = 0; x < vals.length - 1; x++) {
+        extrapolated.push(vals[x + 1] - vals[x]);
+    }
+    chain.push(extrapolated[extrapolated.length - 1]);
+    extrapolate(extrapolated);
+}
+
+let out = 0;
+input.forEach((line) => {
+    line = line.split(' ').reverse().map((val) => {
+        return parseInt(val)
+    });
+    chain = [line[line.length - 1]];
+    extrapolate(line);
+    let adder = 0;
+    chain.forEach((link) => {
+        adder += link;
+    })
+    out += adder;
+});
+
+console.log(out);
