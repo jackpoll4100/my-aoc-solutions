eece3925e57429ca8d0bbb1d7ea069eca34ed1fc
jackpoll4100 <43215677+jackpoll4100@users.noreply.github.com>
2025-09-30 16:51:52 -0600

    Create aoc-day4-p2.py

---
+++ 2021/aoc-day4-p2.py
@@ -0,0 +1,61 @@
+import re
+
+# Put comma separated input here:
+callouts = ""
+callouts = callouts.split(',')
+
+# Put comma separated input here:
+boards = ""
+
+pattern = re.compile(r'(,){2,}')
+boards = re.sub(pattern, ',', boards)
+boards = boards.split(',')
+boardMatrix = []
+tempBoard = []
+tempRow = []
+count = 0
+for x in boards:
+    tempRow.append(x)
+    count += 1
+    if count%5==0:
+        tempBoard.append(tempRow)
+        tempRow = []
+    if count%25==0:
+        count = 0
+        boardMatrix.append(tempBoard)
+        tempBoard = []
+winningBoard = []
+leadingSet = []
+breakOut = False
+for y in callouts:
+    leadingSet.append(y)
+    for z in boardMatrix:
+        i = 0
+        while i < 5:
+            check =  all(item in leadingSet for item in [z[0][i], z[1][i], z[2][i], z[3][i], z[4][i]])
+            if check and z not in winningBoard:
+                winningBoard.append(z)
+            if len(winningBoard) == len(boardMatrix):
+                breakOut = True
+                break
+            i += 1
+        for a in z:
+            check =  all(item in leadingSet for item in a)
+            if check and z not in winningBoard:
+                winningBoard.append(z)
+            if len(winningBoard) == len(boardMatrix):
+                breakOut = True
+                break
+        if breakOut:
+            break
+    if breakOut:
+        break
+winningBoard = winningBoard[-1]
+print(winningBoard)
+print(leadingSet)
+sum = 0
+for b in winningBoard:
+    for c in b:
+        if c not in leadingSet:
+            sum += int(c)
+print(sum*int(leadingSet[-1]))
